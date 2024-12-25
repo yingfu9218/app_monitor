872ad7bf61af2b0abfa07031c5e12a4044cba06b
yingfu.chen <yingfu.chen@izhanchi.com>
2024-12-25 18:46:56 +0800

    feat:提交

--- page/DetailPage.tsx
+++ page/DetailPage.tsx
@@ -11,22 +11,24 @@ const Tab = createMaterialTopTabNavigator();
 function BaseInfoTab({ baseInfo }){
   return (
     <View>
-      <Text>{baseInfo && baseInfo.host_info.hostname}</Text>
-      <Text>{baseInfo && baseInfo.host_info.os}</Text>
-      <Text>{baseInfo && baseInfo.host_info.platform}</Text>
+      <Text>主机名:  {baseInfo && baseInfo.host_info.hostname}</Text>
+      <Text>系统：   {baseInfo && baseInfo.host_info.os}</Text>
+      <Text>平台：   {baseInfo && baseInfo.host_info.platform}</Text>
+      <Text>内存大小：{baseInfo && baseInfo.mem_total_str}</Text>
     </View>
   );
 }
-function CpuInfoTab({ baseInfo,cpuTimeData }){
-  console.log("cpu cpuTimeData");
-  console.log(cpuTimeData);
-
+function CpuInfoTab({ baseInfo,cpuPercent,cpuTimeData }){
+  useEffect(() => {
+    console.log("CpuInfoTab:cpu cpuTimeData");
+    console.log(cpuTimeData);
+  }, [cpuTimeData]);
   return (
     <View>
       {baseInfo && <VictoryPie
         data={[
-          { x: "used", y: baseInfo.cpu_used },
-          { x: "free", y: (100-baseInfo.cpu_used) },
+          { x: "used", y: cpuPercent },
+          { x: "free", y: (100-cpuPercent) },
         ]}
         theme={VictoryTheme.clean}
       />
@@ -34,39 +36,36 @@ function CpuInfoTab({ baseInfo,cpuTimeData }){
 
       }
 
-      {/* <VictoryChart*/}
-      {/*  theme={VictoryTheme.clean}*/}
-      {/*>*/}
-      {/*  <VictoryLine*/}
-      {/*    data={cpuTimeData}*/}
-      {/*  />*/}
-      {/*</VictoryChart>*/}
+       <VictoryChart
+        theme={VictoryTheme.clean}
+      >
+        <VictoryLine
+          data={cpuTimeData}
+        />
+      </VictoryChart>
     </View>
   );
 }
 
-function MemInfoTab({ baseInfo }){
+function MemInfoTab({ baseInfo,memDetail,memPercent,menTimeData }){
+  console.log("MemInfoTab memPercent：");
+  console.log(memPercent);
+  console.log("MemInfoTab memDetail：");
+  console.log(memDetail);
   return (
     <View>
-      {baseInfo && <VictoryPie
+      { memDetail &&  <VictoryPie
         data={[
-          { x: "used:"+baseInfo.mem_used_str, y: baseInfo.mem_info.used },
-          { x: "free:"+baseInfo.mem_free_str, y: baseInfo.mem_info.free },
+          { x: "used:"+memDetail.mem_used_str, y: memPercent },
+          { x: "free:"+memDetail.mem_free_str, y: 100-memPercent },
         ]}
         theme={VictoryTheme.clean}
-      />
-      }
+      />}
       <VictoryChart
         theme={VictoryTheme.clean}
       >
         <VictoryLine
-          data={[
-            { x: 1, y: 2 },
-            { x: 2, y: 3 },
-            { x: 3, y: 5 },
-            { x: 4, y: 4 },
-            { x: 5, y: 7 },
-          ]}
+          data={menTimeData}
         />
       </VictoryChart>
     </View>
@@ -79,10 +78,14 @@ function DetailPage({ route }){
   const [baseInfo,setBaseInfo]=React.useState(null);
   const [cpuTimeData,setCpuTimeData]=React.useState([]);
   const [firstLoad,setFirstLoad]=React.useState(true);
+  const [cpupercent,setCpupercent]=React.useState(0.0);
+  const [memDatail,setMemDatail]=React.useState(null);
+  const [memPercent,setMemPercent]=React.useState(0.0);
+  const [memTimeData,setMemTimeData]=React.useState([]);
   const wsRef=useRef(null);
-
+  const wsUrl='ws://'+route.params.serverConfig.serverAddr+':'+route.params.serverConfig.serverPort+'/ws';
   if(!wsRef.current){
-    wsRef.current = new WebSocket('ws://100.95.155.126:8004/ws');
+    wsRef.current = new WebSocket(wsUrl);
     wsRef.current.onopen = () => {
       // connection opened
       wsRef.current.send('something'); // send a message
@@ -92,10 +95,44 @@ function DetailPage({ route }){
       console.log(e.data);
       const messageData=JSON.parse(e.data);
       console.log(messageData.data);
-      if (messageData.m_type=="cpu_percent"){
-        const newCpuTimeData= cpuTimeData.push({x:messageData.now_time,y:messageData.data });
-        setCpuTimeData(newCpuTimeData);
-        // console.log(cpuTimeData);
+      // 消息类型
+      const mType=messageData.m_type;
+      // 更新cpu使用率信息
+      if (mType=="cpu_percent"){
+        setCpupercent(messageData.data);
+        const addCpuData = { x: messageData.now_time.split(" ")[1], y: messageData.data };
+        setCpuTimeData(cpuTimeData => {
+          let oldcpuTimeData=cpuTimeData;
+          if (oldcpuTimeData.length> 6){
+            oldcpuTimeData=oldcpuTimeData.splice(-5);
+          }
+          return [
+            ...oldcpuTimeData,
+            addCpuData,
+          ];
+        });
+
+      }
+      // 更新内存使用率信息
+      if (mType=="mem_datail"){
+        setMemDatail(memDatail=>{
+          return messageData.data;
+        });
+        let newMemPercent=(messageData.data.mem_info.used/messageData.data.mem_info.total)*100;
+        newMemPercent=Math.round(newMemPercent*100)/100;
+        const addMemData = { x: messageData.now_time.split(" ")[1], y: newMemPercent };
+        setMemPercent(newMemPercent);
+        setMemTimeData(memTimeData => {
+          let oldMemTimeData=memTimeData;
+          if (oldMemTimeData.length> 6){
+            oldMemTimeData=oldMemTimeData.splice(-5);
+          }
+          return [
+            ...oldMemTimeData,
+            addMemData,
+          ];
+        });
+
       }
     };
     wsRef.current.onerror = e => {
@@ -134,10 +171,10 @@ function DetailPage({ route }){
         {(props) => <BaseInfoTab {...props} baseInfo={baseInfo} />}
       </Tab.Screen>
       <Tab.Screen name="cpu"  >
-        {(props) => <CpuInfoTab {...props} baseInfo={baseInfo} cpuTimeData={cpuTimeData} />}
+        {(props) => <CpuInfoTab {...props} baseInfo={baseInfo} cpuPercent={cpupercent} cpuTimeData={cpuTimeData} />}
       </Tab.Screen>
-      <Tab.Screen name="Mem" >
-        {(props) => <MemInfoTab {...props} baseInfo={baseInfo} />}
+      <Tab.Screen name="内存" >
+        {(props) => <MemInfoTab {...props} baseInfo={baseInfo} memDetail={memDatail} memPercent={memPercent} menTimeData={memTimeData} />}
       </Tab.Screen>
     </Tab.Navigator>
   );
